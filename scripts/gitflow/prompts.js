b30debc835d38e481309459dd35c1cc3c03b3a18
Rudin Maxim <94765996+RudinMaxim@users.noreply.github.com>
2024-10-10 01:08:08 +0500

    feat: fx

--- scripts/gitflow/prompts.js
+++ scripts/gitflow/prompts.js
@@ -1,66 +1,53 @@
 const readline = require('readline');
 
 function askQuestion(question, choices = null) {
-  return new Promise((resolve, reject) => {
+  return new Promise((resolve) => {
     const rl = readline.createInterface({
       input: process.stdin,
       output: process.stdout,
     });
 
-    let isDone = false; // Флаг для предотвращения повторного вызова
+    function displayChoices(currentChoice) {
+      console.clear();
+      console.log(question);
+      choices.forEach((choice, index) => {
+        console.log(`${index === currentChoice ? '>' : ' '} ${choice}`);
+      });
+    }
+
+    function cleanup(onKeypress) {
+      process.stdin.setRawMode(false);
+      process.stdin.removeListener('keypress', onKeypress);
+      rl.close();
+    }
 
     if (choices) {
       let currentChoice = 0;
 
-      function displayChoices() {
-        if (isDone) return; // Предотвращаем дальнейшее отображение, если выбор уже сделан
-        console.clear();
-        console.log(question);
-        choices.forEach((choice, index) => {
-          console.log(`${index === currentChoice ? '>' : ' '} ${choice}`);
-        });
-      }
-
-      displayChoices();
+      displayChoices(currentChoice);
 
       readline.emitKeypressEvents(process.stdin);
       process.stdin.setRawMode(true);
 
-      const onKeypress = (str, key) => {
-        if (isDone) return;
-        try {
-          if (key.name === 'up' && currentChoice > 0) {
-            currentChoice--;
-          } else if (key.name === 'down' && currentChoice < choices.length - 1) {
-            currentChoice++;
-          } else if (key.name === 'return') {
-            isDone = true;
-            resolve(choices[currentChoice]);
-            cleanup();
-          }
-        } catch (error) {
-          isDone = true;
-          reject(error);
-          cleanup();
-        } finally {
-          displayChoices();
+      const onKeypress = (_str, key) => {
+        if (key.name === 'up' && currentChoice > 0) {
+          currentChoice--;
+        } else if (key.name === 'down' && currentChoice < choices.length - 1) {
+          currentChoice++;
+        } else if (key.name === 'return') {
+          resolve(choices[currentChoice]);
+          cleanup(onKeypress);
         }
+        displayChoices(currentChoice);
       };
 
       process.stdin.on('keypress', onKeypress);
-
-      function cleanup() {
-        process.stdin.setRawMode(false);
-        process.stdin.removeListener('keypress', onKeypress);
-        rl.close();
-      }
     } else {
       rl.question(question, (answer) => {
-        resolve(answer);
+        resolve(answer.trim());
         rl.close();
       });
     }
   });
 }
-
 module.exports = { askQuestion };
