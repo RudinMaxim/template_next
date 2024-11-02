1c2d542bf86d566c98a849245aa7f8020a7aa6c6
Rudin Maxim <94765996+RudinMaxim@users.noreply.github.com>
2024-11-03 00:20:03 +0500

    add

--- scripts/gitflow/config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  margeBranch: ['main', 'develop', 'staging'],
-  branchTypes: ['feature', 'bugfix', 'hotfix', 'release'],
-  commitTypes: ['feat', 'fix', 'docs', 'style', 'refactor', 'test', 'chore'],
-  choiceTypes: ['branch', 'commit', 'push', 'pr', 'merge', 'undo', 'exit'],
-};
