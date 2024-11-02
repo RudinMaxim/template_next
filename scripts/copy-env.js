a6f5895e12a5c7c5ff5a84403caecaa63ce69311
Rudin Maxim <94765996+RudinMaxim@users.noreply.github.com>
2024-11-03 00:16:14 +0500

    add

--- scripts/copy-env.js
+++
@@ -1,6 +0,0 @@
-const fs = require('fs');
-
-if (!fs.existsSync('.env') && fs.existsSync('.env.example')) {
-  fs.copyFileSync('.env.example', '.env');
-  console.log('.env file created from .env.example');
-}
--- src/shared/config/index.ts
+++
@@ -1,4 +0,0 @@
-import { axiosConfig } from './axios';
-import {METRIKA_CONFIG} from './metrika'
-
-export { axiosConfig, METRIKA_CONFIG as metrikaConfig };
