9f8aa022ceecb819caf27d64c2756241c5f9fce1
Rudin Maxim <94765996+RudinMaxim@users.noreply.github.com>
2025-01-06 00:09:24 +0500

    add

--- src/shared/lib/hooks/index.ts
+++ src/shared/lib/hooks/index.ts
@@ -1,5 +1,4 @@
-import { useContent } from './useContent';
 import { useDebounce } from './useDebounce';
 import { useThrottle } from './useThrottle';
 
-export { useDebounce, useThrottle, useContent };
+export { useDebounce, useThrottle };
